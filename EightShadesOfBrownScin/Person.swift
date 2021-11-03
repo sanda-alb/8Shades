99e39155b67e9434da8f55851dcbf6801e33e0c8
Albrekht Sanda <albrekht@kinoplan.ru>
2021-11-03 18:49:20 +0300

    first commit

---
+++ EightShadesOfBrownScin/Person.swift
@@ -0,0 +1,55 @@
+//
+//  Person.swift
+//  EightShadesOfBrownScin
+//
+//  Created by TK_User on 03.11.2021.
+//
+
+struct Person {
+    let title: String?
+    let url: String?
+   
+}
+
+class ScinColors {
+    
+    static func getPerson() -> [Person] {
+        let baseURL = "https://thatsisterimages.s3.amazonaws.com/wp-content/uploads/2018/04/26160552/shutterstock_"
+        let people = [
+            
+            Person (
+                    title: "Marshmellow clored scin",
+                    url: baseURL + "124438054.jpg"
+                   ),
+            Person (
+                    title: "Pretty pecan brown scin",
+                    url: baseURL + "282904385.jpg"
+                   ),
+            Person (
+                    title: "Peanut butter truffle tone",
+                    url: baseURL + "640005088.jpg"
+                   ),
+            Person (
+                    title: "Alluring amber",
+                    url: baseURL + "119230999.jpg"
+                   ),
+            Person (
+                    title: "Brown caramel scin shade",
+                    url: baseURL + "188674505.jpg"
+                   ),
+            Person (
+                    title: "Milc chocolate shade",
+                    url: baseURL + "400969783.jpg"
+                   ),
+            Person (
+                    title: "Espresso brown scin",
+                    url: baseURL + "333668321.jpg"
+                   ),
+            Person (
+                    title: "Delicious cacao black scin shade",
+                    url: baseURL + "719756656.jpg"
+                   )
+        ]
+        return people
+    }
+}
--- EightShadesOfBrownScin/ViewController.swift
+++ EightShadesOfBrownScin/ViewController.swift
@@ -5,15 +5,77 @@
 //  Created by TK_User on 03.11.2021.
 //
 
+import SnapKit
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    
+    var tableView = UITableView()
+    private let cardView = UIView()
+    private let image = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupAll()
     }
+    
+    private func setupAll() {
+        embedViews()
+        setupAppereance()
+        setupLayout()
+    }
+    
+    private func embedViews() {
+        [tableView,
+         cardView
+            ].forEach {
+                view.addSubview($0)
+            }
+    }
+    
+    private func setupLayout() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.leading.equalToSuperview()
+        }
+        
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setupAppereance() {
+        tableView.rowHeight = 100
+        tableView.backgroundColor = UIColor.white
+        
+        cardView.layer.cornerRadius = 10
+        cardView.backgroundColor = UIColor.white
+        
+        
+    }
+    
+    func configureTableView() {
+//        setTableViewDelegates()
+    }
+    
+//    func setTableViewDelegates() {
+//        tableView.delegate = self
+//        tableView.datasource = self
+//    }
+}
 
-
+  
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
